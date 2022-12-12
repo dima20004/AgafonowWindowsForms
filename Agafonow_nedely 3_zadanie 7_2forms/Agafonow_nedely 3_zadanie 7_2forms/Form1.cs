b33112220652bce64224a6e5e6d33aa35c2a0ada
dima20004 <117287272+dima20004@users.noreply.github.com>
2022-12-12 15:04:07 +0300

    Add files via upload

---
+++ Agafonow_nedely 3_zadanie 7_2forms/Agafonow_nedely 3_zadanie 7_2forms/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agafonow_nedely_3_zadanie_7_2forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myArr = textBox1.Text;
+                string[] myStrings;
+                myStrings = myArr.Split(' ');
+
+                if (Convert.ToInt32(textBox2.Text) < 0)
+                {
+                    MessageBox.Show("Введите корректное число!");
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    return;
+                }
+
+
+                int n = Convert.ToInt32(textBox2.Text);
+
+                for (int i = 0; i < myStrings.Length; i++)
+                {
+                    if (myStrings[i].Length > n)
+                    {
+
+                    }
+                    else
+                    {
+                        textBox3.Text = textBox3.Text + myStrings[i] + " ";
+                    }
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка, введите корректный данные!");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
---
+++ Agafonow_nedely 3_zadanie 7_forms/Agafonow_nedely 3_zadanie 7_forms/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agafonow_nedely_3_zadanie_7_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder a=new StringBuilder(textBox1.Text);
+
+            for (int i = 0; i < a.Length - 1; i = i + 2)
+            {
+                if (a[i] == ' ' || a[i + 1] == ' ')
+                {
+                    i = i - 1;
+                }
+                else
+                {
+                    char buf = a[i];
+                    buf = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = buf;
+                }
+
+            }
+
+            textBox2.Text = Convert.ToString(a);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
